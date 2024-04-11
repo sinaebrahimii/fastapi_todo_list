be42276c66b610ba9aca8a5812211d253112b653
SinEebrahimi <sinaebrahimi31@gmail.com>
2024-04-11 22:21:53 +0330

    first endpoint created

--- routers/admin.py
+++ routers/admin.py
@@ -2,13 +2,13 @@ from fastapi import APIRouter, HTTPException,Depends,Path
 from sqlalchemy.orm import Session
 from typing import Annotated
 from starlette import status
-import models
+from models import Todos
 from database import SessionLocal
 from pydantic import BaseModel,Field
 from .auth import get_current_user
 
 
-router = APIRouter()
+router = APIRouter(prefix='/admin', tags=['admin'])
 
 
 
@@ -21,3 +21,9 @@ def get_db():
 
 db_dependency= Annotated[Session , Depends(get_db)]
 user_dependency= Annotated[dict , Depends(get_current_user)]
+
+@router.get("/todos", status_code=status.HTTP_200_OK)
+async def get_todos(user:user_dependency,db:db_dependency):
+    if user is None or user.get('user_role')!= 'admin':
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Failed to authenticate")
+    return db.query(Todos).all()
